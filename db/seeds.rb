5f69071c7a8696a5d2133bb1f70460542d8b9a2b
bebopvinh <vinhdo07@gmail.com>
2019-05-13 19:53:31 -0700

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+lesson_names = %w(
+   Create-Action
+   Create-Action-Lab
+   Index-Show-New-Create-Lab
+   Edit/Update-Action
+   form_for-on-Edit
+   Strong-Params-Basics
+)
+
+lesson_names.each do |name|
+   Lesson.find_or_create_by(name: name) do |l|
+      l.category = "Rails"
+      l.section = "CRUD with Rails"
+   end
+end
