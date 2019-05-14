36afff1024147b0ee0098eefcc75ca9d02b1fa5f
bebopvinh <vinhdo07@gmail.com>
2019-05-14 12:10:27 -0700

    Removed initial markdown editor residue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :lessons, only: [:show, :index]
+  resources :lessons, only: [:show, :index] do
+    resources :contributions, only: [:new, :create, :edit, :update]
+  end
+  
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
