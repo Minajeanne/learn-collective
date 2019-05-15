924bee6d4f563f8d15bbf82411ebb0970bb4cc70
bebopvinh <vinhdo07@gmail.com>
2019-05-14 20:24:16 -0700

    Reconfigure edit routes for contributions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :lessons, only: [:show, :index] do
-    resources :contributions, only: [:new, :create, :edit, :update]
+    resources :contributions, only: [:create]
   end
   
+  resources :contributions, only: [:edit, :update, :destroy]
   devise_for :users
-
 end
