9a0ba562e9907d6fc0e43144697ffd5dcf9a96a1
bebopvinh <vinhdo07@gmail.com>
2019-05-16 19:06:16 -0700

    More Lesson CRUDs

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,20 @@
 class LessonsController < ApplicationController
-   before_action :find_lesson, only: :show
+   before_action :find_lesson, only: [:show, :edit, :update, :destroy]
+   before_action :authenticate_user!, except: [:index, :show]
+
+   def new
+      @lesson = Lesson.new
+   end
+
+   def create
+      binding.pry
+      @lesson = Lesson.new(lesson_params)
+      if @lesson.save
+         redirect_to lesson_path(@lesson), notice: "Lesson create!"
+      else
+         render :new
+      end
+   end
 
    def index
       @lessons = Lesson.all
@@ -9,9 +24,23 @@ class LessonsController < ApplicationController
       @contribution = Contribution.new
    end
 
+   def edit
+
+   end
+
+   def update
+   end
+
+   def destroy
+   end
+
    private 
       def find_lesson
          @lesson = Lesson.find_by(id: params[:id])
+      end
+
+      def lesson_params
+         params.require(:lesson).permit(:name, :category, :section)
       end
 end
 
